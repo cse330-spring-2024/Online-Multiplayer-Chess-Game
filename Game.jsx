e9cd9459c7d9a368a4ed78e0bb061443f11a56bf
Yanqing509266 <f.yanqing@wustl.edu>
2024-04-21 00:40:43 -0500

    111

--- Game.jsx
+++ Game.jsx
@@ -2,12 +2,12 @@ import "./Game.css"
 import List from "./List.jsx"
 import Play from "./Play.jsx"
 
-function Game({user, setUser}) {
+function Game({ user, setUser, chat_message, setChat_message, game_board, setGame_board, chat_room_name, setChat_room_name,room_list,setRoom_list }) {
     return (
         <>
             <section id="room_section">
-                <List />
-                <Play user={user} setUser={(data) => setUser(data)} chat_message={chat_message} setChat_message={(data) => setChat_message(data)}/>
+                <List chat_room_name={chat_room_name} setChat_room_name={(data) => setChat_room_name(data)} room_list={room_list} setRoom_list={(data) => setRoom_list(data)}/>
+                <Play user={user} setUser={(data) => setUser(data)} chat_message={chat_message} setChat_message={(data) => setChat_message(data)} game_board={game_board} setGame_board={setGame_board} chat_room_name={chat_room_name} setChat_room_name={setChat_room_name} />
             </section>
         </>
     );
--- Window.jsx
+++ Window.jsx
@@ -4,15 +4,18 @@ import Room from "./Room.jsx"
 import Game from "./Game.jsx"
 import { useState } from "react"
 
-function Window(){
+function Window() {
     const [user, setUser] = useState("");
     const [chat_message, setChat_message] = useState([]);
-    return(
-            <section id="main_page">
-                <Info user={user} setUser={(data) => setUser(data)}/>
-                <Room user={user} setUser={(data) => setUser(data)}/>
-                <Game user={user} setUser={(data) => setUser(data)} chat_message={chat_message} setChat_message={(data) => setChat_message(data)}/>
-            </section>
+    const [game_board, setGame_board] = useState([]);
+    const [chat_room_name, setChat_room_name] = useState("");
+    const [room_list, setRoom_list] = useState([]);
+    return (
+        <section id="main_page">
+            <Info user={user} setUser={(data) => setUser(data)} />
+            <Room user={user} setUser={(data) => setUser(data)} chat_room_name={chat_room_name} />
+            <Game user={user} setUser={(data) => setUser(data)} chat_message={chat_message} setChat_message={(data) => setChat_message(data)} game_board={game_board} setGame_board={(data) => setGame_board(data)} chat_room_name={chat_room_name} setChat_room_name={(data) => setChat_room_name(data)} room_list={room_list} setRoom_list={setRoom_list}/>
+        </section>
     );
 }
 
