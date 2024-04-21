02592f487c9de0aae726e62ed607aabff8776d21
Yanqing509266 <f.yanqing@wustl.edu>
2024-04-20 19:26:15 -0500

    11

--- server/server.js
+++ server/server.js
@@ -12,8 +12,7 @@ const room_games = new Map();
 //room_games: players in rooms & the place where the game info is placed
 //room_games={"room_name":["user1", "user2","1","0"],'room_name":["user3", "user4","2","0"]} 
 //player_X username, player_Y username, index in player_X_history_x player_X_history_y,index in player_Y_history_x player_Y_history_y
-const player_X_history = [];//[[0,1,],[2,1,3]];  player_position=0
-const player_O_history = [];//[[1,1,2],[2,1,3]];  player_position=1
+const player_history = [];//[[0,1,2],[2,1,3]];  player_position=0
 io.on('connection', (socket) => {
     //Create room
     //data:roomname, username
@@ -22,12 +21,11 @@ io.on('connection', (socket) => {
             console.log("User " + data + " create room " + data['roomname']);
             //Add roomname map to room_list
             room_list.set(data['roomname'], new Set());
-            let history_len = player_X_history.length;
+            let history_len = player_history.length;
             //Add array to room_games with key roomname
             room_games.set(data['roomname'], ["", "", history_len.toString, "0"]);//No players currently
-            let empty_arr = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-            player_X_history.push(empty_arr);
-            player_O_history.push(empty_arr);
+            let empty_arr = [-1, -1, -1, -1, -1, -1, -1, -1, -1];
+            player_history.push(empty_arr);
             //Let all users know the room is creates
             io.sockets.emit("create_room", { success: true, username: data['username'], roomname: data["roomname"] });
         }
@@ -55,8 +53,7 @@ io.on('connection', (socket) => {
             io.sockets.to(data["username"]).emit("join_room", {
                 current_player_x: room_games.get(data['roomname'])[0], //"user1"
                 current_player_y: room_games.get(data['roomname'])[1], //"user2"
-                player_X_history: player_X_history[parseInt(room_games.get(data['roomname'])[2])],
-                player_O_history: player_O_history[parseInt(room_games.get(data['roomname'])[2])],
+                player_history: player_history[parseInt(room_games.get(data['roomname'])[2])],
                 game_result: room_games.get(data['roomname'])[3],
                 success: true
             });
@@ -138,19 +135,18 @@ io.on('connection', (socket) => {
                 if (parseInt(room_games.get(data['roomname'])[3]) == 0) {
                     //check if the current piece is empty
                     let history_index = parseInt(room_games.get(data['roomname'])[2]);
-                    let game_board_x = player_X_history[history_index];
-                    let game_board_o = player_O_history[history_index];
-                    if (game_board_x[(data['x'] - 1) + (data['y'] - 1) * 3] == 0 && game_board_o[(data['x'] - 1) + (data['y'] - 1) * 3] == 0) {
+                    let game_board = player_history[history_index];
+                    if (game_board[(data['x'] - 1) + (data['y'] - 1) * 3] == -1 ) {
                         //x
                         if(data['player_position']==0){
-                            game_board_x[(data['x'] - 1) + (data['y'] - 1) * 3]=1;
-                            player_X_history[history_index]=game_board_x;
+                            game_board[(data['x'] - 1) + (data['y'] - 1) * 3]=1;
                         }
                         //o
                         else{
-                            game_board_o[(data['x'] - 1) + (data['y'] - 1) * 3]=1;
-                            player_O_history[history_index]=game_board_o;
+                            game_board[(data['x'] - 1) + (data['y'] - 1) * 3]=0;
                         }
+                        player_history[history_index]=game_board;
+                        //check if any player win
 
                     }
                     //this place is not empty
